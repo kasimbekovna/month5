ec3edc54c87298e79cfcc543cf9943962d40fbf2
kasimbekovna <samaraismailova1985@gmail.com>
2024-08-16 14:28:18 +0300

    Ваше сообщение о коммите

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -1,13 +1,20 @@
 # movie_app/urls.py
 from django.urls import path
 from . import views
-
+from .views import (
+    movie_list_api_view,
+    movie_detail_api_view,
+    director_list_api_view,
+    director_detail_api_view,
+    review_list_api_view,
+    review_detail_api_view
+)
 urlpatterns = [
-    path('movies/', views.MovieListAPIView.as_view(), name='movie-list'),
-    path('movies/<int:id>/', views.MovieItemAPIView.as_view(), name='movie-detail'),
-    path('movies/reviews/', views.MovieReviewsListAPIView.as_view(), name='movie-reviews'),
-    path('directors/', views.DirectorListAPIView.as_view(), name='director-list'),
-    path('directors/<int:id>/', views.DirectorItemAPIView.as_view(), name='director-detail'),
-    path('reviews/', views.ReviewListAPIView.as_view(), name='review-list'),
-    path('reviews/<int:id>/', views.ReviewItemAPIView.as_view(), name='review-detail'),
-]
+    path('', views.MovieListAPIView.as_view()),
+    path('movies/', movie_list_api_view, name = 'movie-list-api'),
+    path('movies/<int:id>/', movie_detail_api_view, name='movie-detail'),
+    path('directors/', director_list_api_view, name = 'director-list'),
+    path('directors/<int:id>/', director_detail_api_view, name='director-detail'),
+    path('reviews/', review_list_api_view, name = 'review-list'),
+    path('reviews/<int:id>/', review_detail_api_view, name='review-detail'),
+   ]
